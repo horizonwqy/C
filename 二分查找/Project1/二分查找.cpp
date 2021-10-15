2824708546098fe8454a71aed032f15dbd7f2372
wqy <1159046908@qq.com>
2021-10-16 02:29:36 +0800

    第一次提交代码

---
+++ 二分查找/Project1/二分查找.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 0,1,2,3,4,5,6,7,8,9 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int left = 0, right = sz - 1;
+	int n = 0;
+	scanf("%d", &n);
+	int mid;
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
+		if (n > arr[mid])
+		{
+			left = mid+1;
+		}
+		else if (n < arr[mid])
+		{
+			right = mid;
+		}
+		else
+			break;
+	}
+	if (left > right)
+	{
+		printf("没找到");
+	}
+	else
+	{
+		printf("找到了，下标是：%d",mid);
+	}
+	return 0;
+}
